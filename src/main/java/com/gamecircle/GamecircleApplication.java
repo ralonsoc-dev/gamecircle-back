f3b9625db8023787048a00b1fe8a8bf83a1d956f
ralonsoc-dev <raul.ac-97@hotmail.com>
2024-11-24 23:23:31 +0100

    init

---
+++ src/main/java/com/gamecircle/GamecircleApplication.java
@@ -0,0 +1,13 @@
+package com.gamecircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamecircle/GamecircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamecircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
